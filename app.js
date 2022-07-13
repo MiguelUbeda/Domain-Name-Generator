b178fc66428de4817adf33c7c0d9ee29035e521a
MiguelUbeda <miguelubeda09@hotmail.com>
2022-07-13 21:30:18 +0000

    HTML updated

--- app.js
+++ app.js
@@ -6,15 +6,17 @@ var noun = ['pacom','raquenet', 'pepe'];
 var ext = ['.com', '.net', '.es'];
 
 
-
+let body = document.body 
 for(var obj1 of pronoun){
   for(var obj2 of adj){
     for(var obj3 of noun){
       for(obj4 of ext){
          if(obj3.substring(obj3.length-obj4.length).includes(obj4.substring(1))){
-           console.log(obj1 + obj2 + obj3.slice(0,obj3.length+1-obj4.length) + obj4);
+          let añadir = document.createTextNode(obj1 + obj2 + obj3.slice(0,obj3.length+1-obj4.length) + obj4)
+          body.appendChild(añadir);
          } else {
-           console.log(obj1 + obj2 + obj3 + obj4)
+           let añadir2 = document.createTextNode(obj1 + obj2 + obj3 + obj4)
+           body.appenChild(añadir2)
          }
       }
     }
--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["pacom", "raquenet", "pepe"];
+  var ext = [".com", ".net", ".es"];
+
+  let body = document.body;
+  for (var obj1 of pronoun) {
+    for (var obj2 of adj) {
+      for (var obj3 of noun) {
+        for (var obj4 of ext) {
+          let url = obj3
+            .substring(obj3.length - obj4.length)
+            .includes(obj4.substring(1))
+            ? obj1 + obj2 + obj3.slice(0, obj3.length + 1 - obj4.length) + obj4
+            : obj1 + obj2 + obj3 + obj4;
+          let añadir2 = document.createElement("p");
+          añadir2.innerHTML = url;
+          body.appendChild(añadir2);
+        }
+      }
+    }
+  }
 };
